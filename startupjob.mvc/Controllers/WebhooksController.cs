795ab8dbe7b28f924ee3a63f1e87083e840062a0
Zdeno Sekerák <zdenko.sekerak@elgas.cz>
2021-06-29 14:41:26 +0200

    Pridany projekt startup.mvc. Zlahka prerobeny example.

---
+++ startupjob.mvc/Controllers/WebhooksController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using startupjob.mvc.Models;
+
+namespace startupjob.mvc.Controllers
+{
+    [Route("webhook")]
+    public class WebhooksController : Controller
+    {
+        private readonly SubscribersDbContext _db;
+
+        public WebhooksController(SubscribersDbContext subscribersDb)
+        {
+            // Get instance for the DbContext from the DI
+            _db = subscribersDb;
+        }
+
+        // POST: /
+        [HttpPost]
+        public async Task<IActionResult> ReceiveEventsAsync([FromBody] JObject payload)
+        {
+            // Check if the request paload could be parsed as JSON array
+            if (!ModelState.IsValid)
+                return BadRequest();
+            
+            JObject json = JObject.Parse(payload.ToString());
+
+            // Check if the email already exist
+            string email = (string)json["Email"];
+
+            var subscriber = _db.Subscribers.FirstOrDefault(s => s.Email == email);
+            if (subscriber != null)
+            {
+                // Unsubscribe
+                subscriber.Subscribed = false;
+            }
+
+            // Save changes to the database
+            await _db.SaveChangesAsync();
+            return Ok();
+        }
+
+        // GET: /
+        [Route("/")]
+        [HttpGet]
+        public IActionResult Home()
+        {
+            return Json(new { message = "Webhook for startupjob is up and running. Sending [POST] requests to: '/webhook'." });
+        }
+    }
+}
---
+++ startupjob.mvc/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Http;
+using startupjob.mvc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace startupjob.mvc
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddDbContext<SubscribersDbContext>(builder => builder.UseSqlite("Data Source=EMPY.db")); // SQLite connection string
+            services.AddApplicationInsightsTelemetry();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SubscribersDbContext db)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+            // Run the migrations to update the database to latest version
+            db.Database.Migrate();
+        }
+    }
+}
